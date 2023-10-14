a747e643523c33eb20684457b4ac49e9f3900c07
Ian Anjos <78967763+IanAguiar-ai@users.noreply.github.com>
2023-10-14 12:50:21 -0300

    Fixing dependencies

--- graph_in_ssd1306.py
+++ graph_in_ssd1306.py
@@ -1,4 +1,6 @@
-import gfx #Need this library to run
+"""
+Class to help make graphics on ssd1306
+"""
 
 def normalize_list(y, a, b):
     min_y = min(y)
@@ -13,31 +15,49 @@ def normalize_list(y, a, b):
 
 class Graph:
     def __init__(self, oled, x:list, y:list, steps = 5, dimensions_visor = (128, 64)):
-        self.x_0 = x[0]
-        self.x_1 = x[1]
-        self.y_0 = y[0]
-        self.y_1 = y[1]
+        self.x_0 = min(x)
+        self.x_1 = max(x)
+        self.y_0 = min(y)
+        self.y_1 = max(y)
         self.oled = oled
         self.steps = steps
         self.len_list = int((self.x_1 - self.x_0 + 1)/steps)
         self.y = [0 for i in range(self.len_list)]
         self.x = [i*self.steps + self.x_0 for i in range(self.len_list)]
-        self.graphics = gfx.GFX(*dimensions_visor, oled.pixel) #oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+        if module:
+            self.graphics = gfx.GFX(*dimensions_visor, oled.pixel) #oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
     def add(self, value):
         self.values.append(-value)
         if len(self.y) > self.len_list:
             del self.y[0]
 
+    if module:
+        def pass_to_graph(self, fill = False):
+            self.graphics.rect(self.x_0, self.y_0, self.x_1-self.x_0, self.y_1-self.y_0, 1)
+            self.y_real = normalize_list(self.y, self.y_0, self.y_1)
+            
+            for i in range(len(self.x) - 1):
+                self.graphics.line(self.x[i], self.y_real[i], self.x[i+1], self.y_real[i+1], 1)
+                graphics.circle(self.x[i+1], self.y_real[i+1], max(int(self.steps/3), 7), 1)
+            oled.show()
+            
+            if fill:
+                self.oled.fill(0)
+                
+    def rectangle(self):
+        self.oled.hline(self.x_0, self.y_0, self.x_1-self.x_0, 1)
+        self.oled.hline(self.x_1, self.y_0, self.x_1-self.x_0, 1)
+        self.oled.vline(self.x_0, self.y_0, self.y_1-self.y_0, 1)
+        self.oled.vline(self.x_0, self.y_1, self.y_1-self.y_0, 1)
+            
     def pass_to_graph(self, fill = False):
-        self.graphics.rect(self.x_0, self.y_0, self.x_1-self.x_0, self.y_1-self.y_0, 1)
         self.y_real = normalize_list(self.y, self.y_0, self.y_1)
+            
+            
         for i in range(len(self.x) - 1):
-            self.graphics.line(self.x[i], self.y_real[i], self.x[i+1], self.y_real[i+1], 1)
-            graphics.circle(self.x[i+1], self.y_real[i+1], max(int(self.steps/3), 7), 1)
+            self.oled.line(self.x[i], self.y_real[i], self.x[i+1], self.y_real[i+1], 1)
+                
         oled.show()
         if fill:
             self.oled.fill(0)
-        
-            
-
